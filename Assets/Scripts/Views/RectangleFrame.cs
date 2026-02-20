9ce9c9403151b6038a9a1c1554a3eae61202dc86
Rope <roperaganmen88@gmail.com>
2026-02-20 19:49:27 +0900

    fix: Rectangleのnamesppace欠け修正

--- Assets/Scripts/Views/RectangleFrame.cs
+++ Assets/Scripts/Views/RectangleFrame.cs
@@ -1,53 +1,56 @@
 using UnityEngine;
 
-public class RectangleFrame : MonoBehaviour
+namespace CommonUI.Tutorial.Views
 {
-    [SerializeField, Tooltip("自身のRectTransform")]
-    private RectTransform _rectTransform;
-
-    [SerializeField, Tooltip("ずっと表示されるフレームの座標")]
-    private RectTransform _baseTransform;
-
-    [SerializeField, Tooltip("アニメーションのあるフレームの座標")]
-    private RectTransform _animatedRectTransform;
-
-    /// <summary>
-    /// アニメーションフレームの大きさの初期値(横)
-    /// </summary>
-    private const float AnimatedWidth = 300;
-
-    /// <summary>
-    /// アニメーションフレームの大きさの初期値(縦)
-    /// </summary>
-    private const float AnimatedHeight = 100;
-
-    /// <summary>
-    /// コーチマークの座標に合わせる。
-    /// </summary>
-    /// <param name="coachMarkPos">コーチマークの座標</param>
-    public void SetPosition(RectTransform coachMarkPos)
+    public class RectangleFrame : MonoBehaviour
     {
-        _rectTransform.position = coachMarkPos.position;
-    }
+        [SerializeField, Tooltip("自身のRectTransform")]
+        private RectTransform _rectTransform;
 
-    /// <summary>
-    /// コーチマークのサイズに合わせる。
-    /// </summary>
-    /// <param name="coachMarkPos"></param>
-    public void SetSize(RectTransform coachMarkPos)
-    {
-        // コーチマークのサイズを取得する。
-        var width = coachMarkPos.sizeDelta.x;
-        var height = coachMarkPos.sizeDelta.y;
+        [SerializeField, Tooltip("ずっと表示されるフレームの座標")]
+        private RectTransform _baseTransform;
 
-        // ベースフレームの大きさを設定する。
-        _baseTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
-        _baseTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+        [SerializeField, Tooltip("アニメーションのあるフレームの座標")]
+        private RectTransform _animatedRectTransform;
 
-        // アニメーションフレームの大きさを設定する。
-        // アニメーション内でsizeDeltaは使用されているため、scaleを変更することで大きさを合わせる。
-        var scaleX = width / AnimatedWidth;
-        var scaleY = height / AnimatedHeight;
-        _animatedRectTransform.localScale = new Vector3(scaleX, scaleY, 1);
+        /// <summary>
+        /// アニメーションフレームの大きさの初期値(横)
+        /// </summary>
+        private const float AnimatedWidth = 300;
+
+        /// <summary>
+        /// アニメーションフレームの大きさの初期値(縦)
+        /// </summary>
+        private const float AnimatedHeight = 100;
+
+        /// <summary>
+        /// コーチマークの座標に合わせる。
+        /// </summary>
+        /// <param name="coachMarkPos">コーチマークの座標</param>
+        public void SetPosition(RectTransform coachMarkPos)
+        {
+            _rectTransform.position = coachMarkPos.position;
+        }
+
+        /// <summary>
+        /// コーチマークのサイズに合わせる。
+        /// </summary>
+        /// <param name="coachMarkPos"></param>
+        public void SetSize(RectTransform coachMarkPos)
+        {
+            // コーチマークのサイズを取得する。
+            var width = coachMarkPos.sizeDelta.x;
+            var height = coachMarkPos.sizeDelta.y;
+
+            // ベースフレームの大きさを設定する。
+            _baseTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            _baseTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+
+            // アニメーションフレームの大きさを設定する。
+            // アニメーション内でsizeDeltaは使用されているため、scaleを変更することで大きさを合わせる。
+            var scaleX = width / AnimatedWidth;
+            var scaleY = height / AnimatedHeight;
+            _animatedRectTransform.localScale = new Vector3(scaleX, scaleY, 1);
+        }
     }
 }
