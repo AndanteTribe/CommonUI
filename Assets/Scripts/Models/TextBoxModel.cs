46e3e8096b5bd262767c8bd0fa043062d933f3ea
asahinnnure <136082695+asahinnnure@users.noreply.github.com>
2026-03-10 21:38:15 +0900

    feat: キャラクター設定項目をモデルに追加.

--- Assets/Scripts/Models/TextBoxModel.cs
+++ Assets/Scripts/Models/TextBoxModel.cs
@@ -13,6 +13,13 @@ namespace CommonUI.Tutorial.Models
         {
             _position = TextPositions.MiddleCenter;
         }
+        [SerializeField, Tooltip("表示するキャラクター(設定がない場合には表示なし)")]
+        private GameObject _character;
+        /// <summary>
+        /// 表示するキャラクター
+        /// <remarks>設定がない場合には表示なし</remarks>
+        /// </summary>
+        public GameObject Character => _character;
         [SerializeField, Tooltip("大まかな位置")]
         private TextPositions _position;
         /// <summary>
