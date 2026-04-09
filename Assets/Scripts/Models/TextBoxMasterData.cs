6cb8807dbc4c713302d01db2f13de7ce5e9186d1
Rope <roperaganmen88@gmail.com>
2026-04-09 22:34:00 +0900

    fix: マスターデータ消えるのうざいので修正

--- Assets/Scripts/Models/TextBoxMasterData.cs
+++ Assets/Scripts/Models/TextBoxMasterData.cs
@@ -32,7 +32,10 @@ namespace CommonUI.Tutorial.Models
             {
                 for (var i = _previousCount; i < _models.Length; i++)
                 {
-                    _models[i] = new TextBoxModel();
+                    if (_models[i] == null)
+                    {
+                        _models[i] = new TextBoxModel();
+                    }
                 }
             }
             _previousCount = _models!.Length;
